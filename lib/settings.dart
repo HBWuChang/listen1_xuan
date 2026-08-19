de37d051efcf1a8c8af9bd5131f16513a0733d31
HBWuChang <2243162077@qq.com>
2026-08-19 20:28:35 +0800

    添加一些构建信息 winapkiosmacos

--- lib/settings.dart
+++ lib/settings.dart
@@ -127,11 +127,10 @@ class _LoginWebviewState extends State<LoginWebview> {
           var t = jsonDecode(await widget.controller.getCookies())["cookies"];
           final cookies = t
               .map<Cookie>(
-                (item) =>
-                    Cookie(
-                      item['name'] as String,
-                      Uri.encodeComponent(item['value'] as String),
-                    ),
+                (item) => Cookie(
+                  item['name'] as String,
+                  Uri.encodeComponent(item['value'] as String),
+                ),
               )
               .toList();
           await savePlatformToken(
@@ -716,6 +715,22 @@ class _SettingsPageState extends State<SettingsPage> {
                           return ListTile(
                             leading: Icon(Icons.system_update),
                             title: Text('更新版本'),
+                            trailing: Text(
+                              '当前构建hash：${UpdController.buildGitHash}\n${isAndroid ? 'cronetHttpNoPlay：${UpdController.cronetHttpNoPlay.toString()}；' : ''}${isFfmpegEnabled ? 'FFmpeg已启用' : 'FFmpeg未启用'}',
+                            ),
+                            onLongPress: () {
+                              showInfoSnackbar(
+                                '@DustDot',
+                                'https://github.com/HBWuChang/listen1_xuan/pull/43',
+                                onTap: () {
+                                  g_launchURL(
+                                    Uri.parse(
+                                      'https://github.com/HBWuChang/listen1_xuan/pull/43',
+                                    ),
+                                  );
+                                },
+                              );
+                            },
                           );
                         },
                         canTapOnHeader: true,
