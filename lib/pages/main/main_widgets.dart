154906db491f48408fea1744d6bfef9fd17172ca
EvanChang <evanchang@88vip.site>
2026-08-24 17:42:27 +0800

    这就不需要了。。 winapkiosmacossta

--- lib/pages/main/main_widgets.dart
+++ lib/pages/main/main_widgets.dart
@@ -347,7 +347,6 @@ Listener _mainContent() => Listener(
                     builder = (context_in_1) {
                       return Scaffold(
                         appBar: AppBar(
-                          leading: BackButton(onPressed: routerPop),
                           title: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
